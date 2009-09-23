2f0480e13d768bc3d5d0a39256b722615ba1c7ca
Brian Rosner <brosner@gmail.com>
2009-09-23 16:29:13 -0600

    added setup.py to allow pip install

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+ 
+ 
+setup(
+    name = "dregni",
+    version = "0.1",
+    author = "Marty Alchin",
+    author_email = "gulopine@gamemusic.org",
+    description = "A basic event management application for Django",
+    # long_description = open("README").read(),
+    license = "New BSD",
+    url = "http://code.google.com/p/dregni/",
+    packages = [
+        "dregni",
+        "dregni.templatetags",
+    ],
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+    ]
+)

2d37ab8456e6de9d37ba8f6c8214e8e31126277d
Brian Rosner <brosner@gmail.com>
2009-09-24 02:25:06 -0600

    implemented EventManager.after_date

--- dregni/manager.py
+++ dregni/manager.py
@@ -7,3 +7,9 @@ class EventManager(models.Manager):
     def for_current_site(self):
         return self.filter(site=Site.objects.get_current())
 
+    def after_date(self, reference_day, num_days):
+        queryset = self.filter(start_date__gt=reference_day)
+        if num_days:
+            day_limit = reference_day + datetime.timedelta(days=num_days)
+            queryset = queryset.filter(start_date__lt=day_limit)
+        return queryset

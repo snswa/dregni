7bd1d23ec781e05e0443b8a07ca53f8a1fe7676c
Matthew Scott <gldnspud@gmail.com>
2010-12-31 23:40:13 -0800

    Allow alternate starting queryset for after_date query, so we can use after_date with Events associated with a group.

--- dregni/manager.py
+++ dregni/manager.py
@@ -7,8 +7,9 @@ class EventManager(models.Manager):
     def for_current_site(self):
         return self.filter(site=Site.objects.get_current())
 
-    def after_date(self, reference_day, num_days):
-        queryset = self.filter(start_date__gt=reference_day)
+    def after_date(self, reference_day, num_days, starting_queryset=None):
+        starting_queryset = starting_queryset if starting_queryset is not None else self
+        queryset = starting_queryset.filter(start_date__gt=reference_day)
         if num_days:
             day_limit = reference_day + datetime.timedelta(days=num_days)
             queryset = queryset.filter(start_date__lt=day_limit)

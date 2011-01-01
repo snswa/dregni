2c41bd4326b83ad649a027c4a3fbec6ccd04384b
Matthew Scott <gldnspud@gmail.com>
2010-12-31 21:01:26 -0800

    Initial model revision with south migrations

---
+++ dregni/migrations/0001_initial.py
@@ -0,0 +1,82 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding model 'Event'
+        db.create_table('dregni_event', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('site', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['sites.Site'])),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=50, db_index=True)),
+            ('description', self.gf('django.db.models.fields.TextField')()),
+            ('start_date', self.gf('django.db.models.fields.DateField')()),
+            ('start_time', self.gf('django.db.models.fields.TimeField')(null=True, blank=True)),
+            ('end_date', self.gf('django.db.models.fields.DateField')(null=True, blank=True)),
+            ('end_time', self.gf('django.db.models.fields.TimeField')(null=True, blank=True)),
+            ('tags', self.gf('tagging.fields.TagField')()),
+        ))
+        db.send_create_signal('dregni', ['Event'])
+
+        # Adding unique constraint on 'Event', fields ['start_date', 'slug']
+        db.create_unique('dregni_event', ['start_date', 'slug'])
+
+        # Adding model 'EventMetadata'
+        db.create_table('dregni_eventmetadata', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('event', self.gf('django.db.models.fields.related.ForeignKey')(related_name='metadata', to=orm['dregni.Event'])),
+            ('type', self.gf('django.db.models.fields.CharField')(max_length=20)),
+            ('text', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('url', self.gf('django.db.models.fields.URLField')(max_length=200, blank=True)),
+        ))
+        db.send_create_signal('dregni', ['EventMetadata'])
+
+
+    def backwards(self, orm):
+        
+        # Removing unique constraint on 'Event', fields ['start_date', 'slug']
+        db.delete_unique('dregni_event', ['start_date', 'slug'])
+
+        # Deleting model 'Event'
+        db.delete_table('dregni_event')
+
+        # Deleting model 'EventMetadata'
+        db.delete_table('dregni_eventmetadata')
+
+
+    models = {
+        'dregni.event': {
+            'Meta': {'ordering': "('start_date', 'start_time', 'end_date', 'end_time')", 'unique_together': "(('start_date', 'slug'),)", 'object_name': 'Event'},
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'end_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'end_time': ('django.db.models.fields.TimeField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'site': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['sites.Site']"}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'db_index': 'True'}),
+            'start_date': ('django.db.models.fields.DateField', [], {}),
+            'start_time': ('django.db.models.fields.TimeField', [], {'null': 'True', 'blank': 'True'}),
+            'tags': ('tagging.fields.TagField', [], {}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'dregni.eventmetadata': {
+            'Meta': {'object_name': 'EventMetadata'},
+            'event': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'metadata'", 'to': "orm['dregni.Event']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'text': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'type': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'})
+        },
+        'sites.site': {
+            'Meta': {'ordering': "('domain',)", 'object_name': 'Site', 'db_table': "'django_site'"},
+            'domain': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        }
+    }
+
+    complete_apps = ['dregni']
